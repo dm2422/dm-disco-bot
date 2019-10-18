ea8a43fc1fd472daa204d57d46309737f5623761
daichi mori <dmori841@yahoo.co.jp>
2019-10-18 19:50:55 +0900

    first commit

---
+++ disco/main.js
@@ -0,0 +1,21 @@
+const token='NjM0MzMzNDI1MDk1OTMzOTYy.Xal5jA.JtdqRI5RtKZLWPWLffc68phE8jg'
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('OKOKOK!!!!');
+});
+
+client.login(token);
+
+client.on('message', message => {
+    if (message.content === "!ping"){
+        message.channel.send("PONG");
+    }
+
+    if (message.content==="!today"){
+        const date = new Date();
+        message.channel.send(date.toLocaleDateString("ja-JP"));
+    }
+    console.log("   DISCO: message processed   ");
+});

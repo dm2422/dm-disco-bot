5166d948a5611a6a51f924a2ea7ab5e20c9cfafc
daichi mori <dmori841@yahoo.co.jp>
2019-11-10 21:20:49 +0900

    made command variable

--- disco/main.js
+++ disco/main.js
@@ -1,7 +1,6 @@
 
 const Discord = require('discord.js');
-const {prefix, config} = require('./config.json');
-const token=config.token;
+const {prefix, token} = require('./config.json');
 
 const client = new Discord.Client();
 //const tools = require("./tools");
@@ -14,21 +13,29 @@ client.once('ready', () => {
 client.on('message', message => {
     if (!message.content.startsWith("!") || message.author.bot) return;
 
-    
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
 
     if (message.content === `${prefix}ping`){
         message.channel.send("PONG");
     }
 
-    if (message.content===`${prefix}today`){
+    else if (message.content===`${prefix}today`){
         const date = new Date();
         message.channel.send(date.toLocaleDateString("ja-JP"));
     }
     
-    if (message.content===`${prefix}mods`){
+    else if (message.content===`${prefix}mods`){
         message.channel.send("Not Ready Yet!");
     }
-    console.log("   DISCO: message processed   ");
+
+    else if (command === "args-info"){
+       if (!args.length) {
+           message.channel.send(`Enter args! ${message.author}`);
+       } 
+       message.channel.send(`Command: ${command}\nArgs: ${args}`);
+    }
+    console.log(`   DISCO: message processed   ${message}`);
 });
 
 client.login(token)

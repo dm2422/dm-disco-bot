647a9336671e3daf2ea8a0f46a7152213c16adc2
daichi mori <dmori841@yahoo.co.jp>
2019-11-23 20:41:37 +0900

    fixed getfiles.js

--- tools/getFiles.js
+++ tools/getFiles.js
@@ -6,13 +6,12 @@ module.exports = {
     const list = fs
       .readdirSync(`${cur_dir}/${folder}`, 'utf-8')
       .filter(file => file.endsWith(`.${ext}`));
+
     const List = [];
-    for (const file of [
-      list.array.forEach(element => {
-        element.replace(`.${ext}`, '');
-      }),
-    ]) {
-      List[file] = require(`${cur_dir}/${folder}/${file}`);
+    for (const file of list) {
+      List[
+        file.replace(`.${ext}`, '')
+      ] = require(`${cur_dir}/${folder}/${file}`);
     }
     return List;
   },
